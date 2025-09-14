044a07013d982bb46d538502be5644d998c6b972
zeroest <zeroest.me@gmail.com>
2025-09-15 01:58:40 +0900

    Login form (/w axios)

--- vue3-form/src/App.vue
+++ vue3-form/src/App.vue
@@ -1,5 +1,8 @@
 <template>
-  <form action="">
+  <!-- event.preventDefault 대신 vue 에서 제공해주는 기능 -->
+  <!-- 디렉티브의 이벤트 모디파이어 -->
+  <!-- 이벤트에 대한 동작을 제어 -->
+  <form action="" @submit.prevent="submitForm">
     <div>
       <label for="userId">ID:</label>
       <input id="userId" type="text" v-model="username">
@@ -13,6 +16,8 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   data() {
     return {
@@ -20,6 +25,24 @@ export default {
       password: ''
     }
   },
+  methods: {
+    submitForm(/*event*/) {
+      // form 기본 기능인 submit 되었을때 새로고침 방지
+      // event.preventDefault();
+      console.log('submitted')
+      console.log(`username: ${this.username}`)
+      console.log(`password: ${this.password}`)
+
+      // https://jsonplaceholder.typicode.com/
+      const data = {
+        username: this.username,
+        password: this.password
+      };
+      axios.post('https://jsonplaceholder.typicode.com/users', data).then(response => {
+        console.log(response)
+      })
+    }
+  }
 }
 </script>
 
