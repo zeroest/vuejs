62d113b83465c6b705af65983408d5649945c256
zeroest <zeroest.me@gmail.com>
2025-09-18 02:28:17 +0900

    options api vs composition api

--- vue-basic/board-sample/src/App.vue
+++ vue-basic/board-sample/src/App.vue
@@ -1,7 +1,18 @@
-<script setup></script>
+<script setup>
+import CounterCompositionApi from '@/components/CounterCompositionApi.vue';
+import CounterOptionsApi from '@/components/CounterOptionsApi.vue';
+</script>
 
 <template>
 	<div>Hello Vue3</div>
+
+	<hr />
+
+	<CounterCompositionApi></CounterCompositionApi>
+
+	<hr />
+
+	<CounterOptionsApi></CounterOptionsApi>
 </template>
 
 <style></style>
