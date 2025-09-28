b3d03162edb067f4de7228edcd30bdd02f006409
zeroest <zeroest.me@gmail.com>
2025-09-28 15:43:17 +0900

    components

--- vue-basic/board-sample/src/main.js
+++ vue-basic/board-sample/src/main.js
@@ -1,8 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import GlobalComponent from '@/components/GlobalComponent.vue';
 
 import { createApp } from 'vue';
 import App from './App.vue';
 
-createApp(App).mount('#app');
-
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+let app = createApp(App);
+/* 현재 Vue 애플리케이션에서 전역적으로 사용할 수 있도록 전역 등록 */
+app.component('GlobalComponent', GlobalComponent);
+app.mount('#app');
