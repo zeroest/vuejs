1a147e9fd0fefc2e1bd71b98fb39cf139e4c5459
zeroest <zeroest.me@gmail.com>
2025-10-02 00:08:35 +0900

    bootstrap revert

---
+++ vue-basic/board-sample/src/components/13-bootstrap/BootstrapMain.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<GlobalComponent></GlobalComponent>
+		<NavbarComponent></NavbarComponent>
+		<GridComponent></GridComponent>
+	</div>
+</template>
+
+<script>
+import NavbarComponent from '@/components/13-bootstrap/NavbarComponent.vue';
+import GridComponent from '@/components/13-bootstrap/GridComponent.vue';
+
+export default {
+	components: { GridComponent, NavbarComponent },
+	setup() {
+		return {};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
